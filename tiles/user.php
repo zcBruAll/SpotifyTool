9cbd351b28887340a18d1c9288aee0f5a37d5bc7
zcAllBru <allan.brunner@zeacold.com>
2024-05-26 13:32:48 +0200

    Extracted the js into specific files

--- tiles/user.php
+++ tiles/user.php
@@ -10,42 +10,4 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    function getAccessToken() {
-        return $.ajax({
-            url: 'get_access_token.php',
-            method: 'GET',
-            dataType: 'json'
-        });
-    }
-
-    $(document).ready(function() {
-        // Function to update player information
-        getAccessToken().then(function(data) {
-            const accessToken = data.access_token;
-
-            $.ajax({
-                url: 'https://api.spotify.com/v1/me', // Endpoint to fetch current track information
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + accessToken
-                },
-                success: function(response) {
-                    // Update player interface with track information
-                    $('#display-name').text(response?.display_name);
-
-                    $('#profile-pic').attr('src', response?.images[1].url);
-
-                    $('#email').text(response?.email);
-
-                    $('#product').text(response?.product);
-                },
-                error: function(xhr, status, error) {
-                    console.error('Error getting user info:', error);
-                }
-            });
-        }).catch(function(error) {
-            console.error('Error getting access token:', error);
-        });
-    });
-</script>
+<script src="js/tiles/user.js"></script>
